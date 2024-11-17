60fca8402f6adc03bbdd0f7e1a2c9b347735e55e
maiklshetinin <sh_chetinin@mail.ru>
2024-11-17 11:25:01 +0300

    feat: add gh-pages script

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/rick-and-morty-vue-task/", // замените на имя вашего репозитория
   plugins: [vue()],
   resolve: {
     extensions: [".js", ".ts", ".vue"],
